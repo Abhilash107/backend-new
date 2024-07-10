98c0d9e9e25332e0d545d293a70e45bf1805ca21
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-10 22:21:04 +0530

    commits

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 import { ApiError } from "../utils/ApiError.js";
 import { User} from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'; 
 import { ApiResponse } from "../utils/ApiResponse.js";
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -401,3 +401,5 @@ export {
     deleteVideo,
     togglePublishStatus
 }
+
+
