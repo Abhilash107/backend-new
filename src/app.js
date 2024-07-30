d2d1a045acf09ed30de1591b9ba5028315366600
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-31 00:40:38 +0530

    add routes in app.js

--- src/app.js
+++ src/app.js
@@ -17,28 +17,28 @@ app.use(express.static("public"));
 app.use(cookieParser());// to get access and set cookies of the client's browser
 
 //routes import
-// import userRouter from "../routes/user.routes.js";
-// import healthcheckRouter from "../routes/healthcheck.routes.js"
-// import tweetRouter from "../routes/tweet.routes.js"
-// import subscriptionRouter from "../routes/subscription.routes.js"
-// import videoRouter from "../routes/video.routes.js"
-// import commentRouter from "../routes/comment.routes.js"
-// import likeRouter from "../routes/like.routes.js"
-// import playlistRouter from "../routes/playlist.routes.js"
-// import dashboardRouter from "../routes/dashboard.routes.js"//also works for other routes
+import userRouter from "../routes/user.routes.js";
+import healthCheckRouter from "../routes/healthCheck.routes.js"
+import tweetRouter from "../routes/tweet.routes.js"
+import subscriptionRouter from "../routes/subscription.routes.js"
+import videoRouter from "../routes/video.routes.js"
+import commentRouter from "../routes/comment.routes.js"
+import likeRouter from "../routes/like.routes.js"
+import playlistRouter from "../routes/playlist.routes.js"
+import dashboardRouter from "../routes/dashboard.routes.js"//also works for other routes
 // /api/v1/user ==> prefix , gives control to userRouter
 // useRouter ==> it is posted on route '/register' as defined in router.js file 
 
 
-// app.use("/api/v1/healthcheck", healthcheckRouter)
-// app.use("/api/v1/users", userRouter)
-// app.use("/api/v1/tweets", tweetRouter)
-// app.use("/api/v1/subscriptions", subscriptionRouter)
-// app.use("/api/v1/videos", videoRouter)
-// app.use("/api/v1/comments", commentRouter)
-// app.use("/api/v1/likes", likeRouter)
-// app.use("/api/v1/playlist", playlistRouter)
-// app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthCheck", healthCheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 //for register 
 //   http://localhost:8000/api/v1/users/register
