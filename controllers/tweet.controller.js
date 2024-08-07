b71448f3c1a62e73903011764c09e07b7ea7b9d1
Abhilash107 <abhilash00003108884@gmail.com>
2024-08-07 15:26:49 +0530

    fixed bugs

--- controllers/tweet.controller.js
+++ controllers/tweet.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose"
 import { Tweet } from "../models/tweet.models.js"
+import { User } from "../models/user.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -112,9 +114,114 @@ const deleteTweet = asyncHandler(async (req, res) => {
     )
 })
 
+
 //* pipeline
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const {userId} = req.params
+
+    if(!userId){
+        throw new ApiError(400, "Invalid userId")
+    }
+
+    const user = await User.findById(userId)
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1
+                        }
+                    }
+                ]
+            }
+        },
+            //* get the likes of the tweet
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as: "likeDetails",
+                pipeline: [
+                    {
+                        $project:{
+                            likedBy: 1
+                        }
+                    }
+                ]
+                
+            }
+        },
+
+        {
+            $addFields:{
+                likesCount: {
+                    $size: "$likeDetails"
+                },
+
+                ownerDetails: {
+                    $first: "$ownerDetails"
+                },
+
+                isLiked: {
+                    $cond:{
+                        if:{
+                            $in:[new mongoose.Types.ObjectId(req.user?._id), "$likeDetails.likedBy"],
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            }
+        },
+
+        {
+            $sort:{
+                createdAt: -1
+            }
+        },
+
+        {
+            $project: {
+                content: 1,
+                ownerDetails: 1,
+                createdAt: 1,
+                likesCount: 1,
+                isLiked: 1
+
+
+            }
+        }
+    ])
+
+    if(!tweets?.length){
+        new ApiResponse(404, [], "No tweets found")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweets, "Tweets fetched successfully")
+    )
+
+
+
 })
 
 export {
